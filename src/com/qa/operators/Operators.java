6ef48e072b10ea33204b2dcb9e83c939a7018a15
Toksylicious <tokeonas@outlook.com>
2022-06-16 00:34:24 +0100

    OA:Operators

---
+++ src/com/qa/operators/Operators.java
@@ -0,0 +1,25 @@
+package com.qa.operators;
+
+public class Operators {
+
+	// Add
+	public void add(int numberOne, int numberTwo) {
+		System.out.println(numberOne + numberTwo);
+	}
+
+	// Multiply
+	public void multiply(int numberOne, int numberTwo) {
+		System.out.println(numberOne * numberTwo);
+	}
+
+	// Subtract
+	public void subtract(int numberOne, int numberTwo) {
+		System.out.println(numberOne - numberTwo);
+	}
+
+	
+	// Divide
+	public void divide (double numberOne, double numberTwo) {
+		System.out.println(numberOne / numberTwo);
+	}
+}
---
+++ src/com/qa/operators/Runner.java
@@ -0,0 +1,22 @@
+package com.qa.operators;
+
+public class Runner {
+	
+	public static void main(String[] args) {
+		Operators calculate = new Operators();  
+		
+		calculate.add(14,13);
+		calculate.multiply(50,7);
+		calculate.subtract(550,250);
+		calculate.divide(14,3);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
